f37c5204d1d85c2eed68ba12b283e722975086fa
Иван <ivanmatuyhin@gmail.com>
2024-02-28 01:58:18 +0300

    Task-repair

---
+++ src/components/post/post.tsx
@@ -0,0 +1,23 @@
+import {FC, ReactElement} from "react";
+import dayjs from 'dayjs'
+import './post.css'
+
+type PostProps = {
+  title: string,
+  body: string,
+  createdAt: number,
+};
+
+const Post: FC<PostProps> = ({title, body, createdAt}): ReactElement => {
+  const date = dayjs(createdAt).format('DD-MM-YYYY HH:mm');
+
+  return (
+    <div className="PostCard">
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <p>{date}</p>
+    </div>
+  );
+};
+
+export default Post;
