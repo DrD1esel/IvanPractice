b7da0d9c4c6ebd648dfa48516a3ac9317f8290b8
Иван <ivanmatuyhin@gmail.com>
2024-02-29 18:00:09 +0300

    Css for header

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import SinglePostPage from './pages/SinglePostPage/SinglePostPage.tsx';
 export const App = () => {
   return (
     <BrowserRouter>
-      <header>
+      <header className='appHeader'>
         <img
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
           width='48'
