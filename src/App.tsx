5c4fedf465dc7a5405f1e6f42cd657d58e6a46dc
Yauhen Kalanitski <ykalanitski@ibagroup.eu>
2024-03-01 17:43:16 +0300

    Go home on title click

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import "./App.css";
 import MainPage from './pages/MainPage/MainPage.tsx';
 import PostsPage from './pages/PostsPage/PostsPage.tsx';
 import SinglePostPage from './pages/SinglePostPage/SinglePostPage.tsx';
 
 export const App = () => {
+  const navigate = useNavigate()
   return (
-    <BrowserRouter>
+    <>
       <header className='appHeader'>
         <img
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
           width='48'
           height='40'
         />
-        <h1 style={{marginLeft: "20px"}}>IvanPractice</h1>
+        <h1 onClick={() => navigate('/')}>IvanPractice</h1>
       </header>
         <Routes>
           <Route path="/" Component={MainPage} />
           <Route path="/posts" Component={PostsPage} />
           <Route path="/posts/:postId" Component={SinglePostPage} />
         </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
